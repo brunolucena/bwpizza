6a8ff4b69160ba6ce3851175209e53ada6b3aac2
Bruno Lucena <bruno.lucenac@gmail.com>
2020-07-12 12:28:20 -0300

    Criar Estrutura do app. Componentes e Rotas.
        - A rota principal cai no início do pedido, para escolher o recheio.
          A partir dessa rota troca somente o conteúdo do formulário para
          avançar no pedido -> EscolherTamanho e EscolherMassa.
        - EscolherMassa é o último passo, após confirmar, caso retorne com
          sucesso, redireciona para OrderSuccess.

---
+++ src/app/OrderPizza/EscolherRecheio/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.scss";
+
+interface Props {}
+
+const EscolherRecheio: React.FC<Props> = () => {
+  return (
+    <section className="escolher-recheio-container">EscolherRecheio</section>
+  );
+};
+
+export default EscolherRecheio;
---
+++ src/app/OrderSuccess/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.scss";
+
+interface Props {}
+
+const OrderSuccess: React.FC<Props> = () => {
+  return <section className="order-success-container">OrderSuccess</section>;
+};
+
+export default OrderSuccess;
