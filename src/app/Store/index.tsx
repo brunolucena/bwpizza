924093b4903cf0fb0f5d3a73a8f0c23636046de7
Bruno Lucena <bruno.lucenac@gmail.com>
2020-07-12 21:05:12 -0300

    Colocar duck orderPizza na store.

--- src/app/Store/index.tsx
+++ src/app/Store/index.tsx
@@ -1,14 +1,17 @@
+import orderPizza, { State as OrderPizzaState } from "./Ducks/orderPizzaDuck";
 import pizzas, { State as PizzasState } from "./Ducks/pizzasDuck";
 import snackbar, { State as SnackbarState } from "./Ducks/snackbarDuck";
 
 // Interface da store. Todas as redux (ducks) devem ter seu State declarado aqui.
 export interface BWPizzaStore {
+  orderPizza: OrderPizzaState;
   pizzas: PizzasState;
   snackbar: SnackbarState;
 }
 
 // Objeto da store (redux).
 const reducers: any = {
+  orderPizza,
   pizzas,
   snackbar,
 };
